889d02827b7b2d3b563cdcb8404a4194775d6423
egonio <eldon.gonio@gmail.com>
2016-09-10 18:15:42 -0400

    stuff

---
+++ javascripts/main.js
@@ -0,0 +1,10 @@
+console.log('This would be the main JS file.');
+
+
+// changes the main preview
+//x is the main img preview
+//y is the thumbnail that mouse is pointing at
+function changePreview( x,  y){
+
+	getElementById(x).src=getElementById(y).src
+}
