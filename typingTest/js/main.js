89f6d6b424c4ca8f7b0978386de0c479905c5ae8
egonio <eldon.gonio@gmail.com>
2017-03-25 13:29:42 -0400

    added typing test

---
+++ typingTest/js/main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('WPMApp', [])
+
+.controller('FormController', ['$scope', function($scope) {
+
+  $scope.details = { age: "",hours :"",phone:"",avgText :""};
+
+}])
+
+;
+
+
+var phrase51 = [
+  "Jim quickly realized that the beautiful gowns are expensive.",
+  "Quirky spud boys can jam after zapping five worthy Polysixes.",
+  "Jackie will budget for the most expensive zoology equipment."
+];
+
+var phrase50 = [
+  "Zack Gappow saved the job requirement list for the six boys.",
+  "Zelda might fix the job growth plans very quickly on Monday. ",
+  "The wizard quickly jinxed the gnomes before they vaporized. "
+];
+
+var phrase49 =[
+  "All questions asked by five watched experts amaze the judge.",
+  "A quick movement of the enemy will jeopardize six gunboats.",
+  "Just keep examining every low bid quoted for zinc etchings."
+];
