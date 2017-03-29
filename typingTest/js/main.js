1954ce7c04ee20b68bbe3c159721d4347f4a9628
Eldon Gonio <Eldon Gonio>
2017-03-28 21:13:45 -0400

    added a bunch of stuff

--- typingTest/js/main.js
+++ typingTest/js/main.js
@@ -6,6 +6,65 @@ angular.module('WPMApp', [])
 
   $scope.details = { age: "",hours :"",phone:"",avgText :""};
 
+  var temp = $scope.details.age.split(" ");
+  console.log (temp.toString());
+  console.log ($scope.details);
+  console.log ($scope.details.age);
+  $scope.words = temp[0];
+}])
+
+.controller('lightThemeController', ['$scope', function($scope) {
+
+  var words = ['Jim','quickly','realized','that','the','beautiful','gowns','are','expensive'];
+
+  $scope.index = 0;
+  $scope.right = 0;
+  $scope.wrong = 0;
+  $scope.count = 0;
+  $scope.word1 = words[0];
+  $scope.word2 = words[1];
+  $scope.word3 = words[2];
+  $scope.word4 = words[3];
+  $scope.word5 = words[4];
+  $scope.word6 = words[5];
+  $scope.word7 = words[6];
+  $scope.word8 = words[7];
+  $scope.word9 = words[8];
+  $scope.typed = '';
+  $scope.class1="bold";
+
+
+  $scope.typing = function(){
+    $scope.count++;
+
+    console.log($scope.typed.localeCompare(words[$scope.index]));
+    if($scope.typed.includes(" ") || $scope.typed.localeCompare(words[$scope.index]) == 0){
+        console.log($scope.typed.localeCompare(words[$scope.index]));
+        console.log($scope.typed);
+        console.log(words[$scope.index]);
+      if($scope.typed.localeCompare(words[$scope.index]) == 0) {
+        $scope.right++;
+      }else{
+        $scope.wrong++;
+      }
+      $scope.index++;
+      $scope.typed = "";
+      indexBolder($scope.index);
+    }
+
+  };
+
+  function indexBolder(index){
+    index == 0 ? $scope.class1 = "bold" :$scope.class1 = "";
+    index == 1 ? $scope.class2 = "bold" :$scope.class2 = "";
+    index == 2 ? $scope.class3 = "bold" :$scope.class3 = "";
+    index == 3 ? $scope.class4 = "bold" :$scope.class4 = "";
+    index == 4 ? $scope.class5 = "bold" :$scope.class5 = "";
+    index == 5 ? $scope.class6 = "bold" :$scope.class6 = "";
+    index == 6 ? $scope.class7 = "bold" :$scope.class7 = "";
+    index == 7 ? $scope.class8 = "bold" :$scope.class8 = "";
+    index == 8 ? $scope.class9 = "bold" :$scope.class9 = "";
+  }
 }])
 
 ;
